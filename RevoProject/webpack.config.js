b084a9ab9dbe5e63e498fd02ea84f2932b7f7c1e
ittnw39 <ittnw@gmail.com>
2025-10-10 03:05:42 +0900

    Fix webpack publicPath and add favicon for GitHub Pages

--- RevoProject/webpack.config.js
+++ RevoProject/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'web-build'),
     filename: 'bundle.js',
-    publicPath: '/',
+    publicPath: '/Revo/',
   },
   devServer: {
     static: path.join(__dirname, 'public'),
