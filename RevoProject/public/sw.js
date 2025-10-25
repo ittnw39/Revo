08f1efe1a4417d199cb6ead434ba15bd904d396d
ittnw39 <ittnw@gmail.com>
2025-10-26 02:59:00 +0900

    251026: 프로토타입 시연용 PWA 설정 테스트

---
+++ RevoProject/public/sw.js
@@ -0,0 +1,53 @@
+// Service Worker for PWA
+const CACHE_NAME = 'revo-project-v1';
+const urlsToCache = [
+  '/',
+  '/static/js/bundle.js',
+  '/static/css/main.css',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Install event - cache resources
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: Install');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Service Worker: Caching files');
+        return cache.addAll(urlsToCache);
+      })
+      .catch((error) => {
+        console.log('Service Worker: Cache failed', error);
+      })
+  );
+});
+
+// Fetch event - serve from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker: Activate');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Service Worker: Deleting old cache', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
